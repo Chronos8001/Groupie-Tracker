d1ee60bd878778075a5e70a626f3589afbc60ce5
Chronos8001 <antoine.rudnianyn@ynov.com>
2026-01-12 09:32:14 +0100

    added fetch rela dates locat

--- models/client.go
+++ models/client.go
@@ -32,3 +32,63 @@ func FetchArtists() ([]Artist, error) {
 
 	return artists, nil
 }
+
+func FetchLocation() ([]Artist, error) {
+	resp, err := httpClient.Get(baseURL + "/artists")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("API error: " + resp.Status)
+	}
+
+	var LocationsURL []Artist
+	err = json.NewDecoder(resp.Body).Decode(&LocationsURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return LocationsURL, nil
+}
+
+func FetchDates() ([]Artist, error) {
+	resp, err := httpClient.Get(baseURL + "/artists")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("API error: " + resp.Status)
+	}
+
+	var DatesURL []Artist
+	err = json.NewDecoder(resp.Body).Decode(&DatesURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return DatesURL, nil
+}
+
+func FetchRelations() ([]Artist, error) {
+	resp, err := httpClient.Get(baseURL + "/artists")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("API error: " + resp.Status)
+	}
+
+	var RelationsURL []Artist
+	err = json.NewDecoder(resp.Body).Decode(&RelationsURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return RelationsURL, nil
+}
