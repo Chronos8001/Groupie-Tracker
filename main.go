1814c1f510d21fe62cba734a2c568995bfb5d73e
Chronos8001 <antoine.rudnianyn@ynov.com>
2026-01-12 11:35:27 +0100

    add test text on fyne

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
-package fyne
+package main
 
 import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	groupie := app.New()
 	w := groupie.NewWindow("Groupie Tracker")
+	green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+	text1 := canvas.NewText("Hello", green)
+	text2 := canvas.NewText("There", green)
+	text3 := canvas.NewText("General Kenobi", green)
+
+	text2.Move(fyne.NewPos(20, 20))
+	text3.Move(fyne.NewPos(20, 40))
+	content := container.NewWithoutLayout(text1, text2, text3)
 
 	w.SetContent(widget.NewLabel("Welcome to Groupie Tracker!"))
+	w.SetContent(content)
 	w.ShowAndRun()
 }
